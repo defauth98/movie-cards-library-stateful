cdcd1ac966193ef7ebdfb807b186a43e04658b14
defauth98 <neto.daniribeiro@gmail.com>
2021-07-19 10:56:30 -0300

    Updates

---
+++ precache-manifest.35ae91f48f351edd56ad967df35992bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d957368f61ea0e2b8b117816c0ab0a",
+    "url": "/movie-cards-library-stateful/index.html"
+  },
+  {
+    "revision": "03e678df00041de55f42",
+    "url": "/movie-cards-library-stateful/static/css/main.9608a9c1.chunk.css"
+  },
+  {
+    "revision": "7b6f8da0325f9381a435",
+    "url": "/movie-cards-library-stateful/static/js/2.accb46bd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie-cards-library-stateful/static/js/2.accb46bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e678df00041de55f42",
+    "url": "/movie-cards-library-stateful/static/js/main.4026c013.chunk.js"
+  },
+  {
+    "revision": "50df6d9da628b473ec47",
+    "url": "/movie-cards-library-stateful/static/js/runtime-main.57846539.js"
+  }
+]);
